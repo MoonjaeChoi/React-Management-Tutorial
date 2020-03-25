12840eb7adb39ed2ba8dddd02272b687b8c04eb4
MoonjaeChoi <37010595+MoonjaeChoi@users.noreply.github.com>
2020-03-25 23:31:33 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React MJCHOI 2
-        </a>
-      </header>
-    </div>
-  );
+const customers = [
+ {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '손흥민',
+  'birthday': '9615323',
+  'gender': '남자',
+  'job': '프로그래머'
+}
+,{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '유재석',
+  'birthday': '9715323',
+  'gender': '남자',
+  'job': '디자이너'
+}
+,{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '김연아',
+  'birthday': '9815323',
+  'gender': '여자',
+  'job': '복학생'
+}
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+              />
+            )
+          }
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
