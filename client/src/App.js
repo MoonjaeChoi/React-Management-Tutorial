ffdd94c1962092e688acf860afc695e328801ecf
MoonjaeChoi <37010595+MoonjaeChoi@users.noreply.github.com>
2020-04-07 20:10:04 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import './App.css';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -13,48 +14,34 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: 'auto'
   },
   table: {
     minWidth: 1080
   },
   progress: {
-    margin: theme.spacing.unit * 2
+    margin: theme.spacing(2)
   }
 })
 
-const customers = [
- {
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '손흥민',
-  'birthday': '9615323',
-  'gender': '남자',
-  'job': '프로그래머'
-}
-,{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '유재석',
-  'birthday': '9715323',
-  'gender': '남자',
-  'job': '디자이너'
-}
-,{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '김연아',
-  'birthday': '9815323',
-  'gender': '여자',
-  'job': '복학생'
-}
-]
-
 class App extends Component {
-  state = {
-    customer: "",
-    completed: 0
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: '',
+      completed: 0
+    }
+  }
+
+  stateRefresh = () => {
+    this.setState({
+      customers: '',
+      completed: 0
+    });
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
   }
 
   componentDidMount() {
@@ -78,28 +65,31 @@ class App extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableHead>
-          <TableBody>
-            { this.state.customers ? this.state.customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.NAME} birthday={c.birthday} gender={c.gender} job={c.job}/>)}) 
-                                   : 
-                                   <TableRow>
-                                     <TableCell colSpan="6" align="center">
-                                       <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
-                                     </TableCell>
-                                   </TableRow>
-            }
-          </TableBody>
-        </Table>
-      </Paper>
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableHead>
+            <TableBody>
+              { this.state.customers ? this.state.customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.NAME} birthday={c.birthday} gender={c.gender} job={c.job}/>)}) 
+                                    : 
+                                    <TableRow>
+                                      <TableCell colSpan="6" align="center">
+                                        <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                                      </TableCell>
+                                    </TableRow>
+              }
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd stateRefresh={this.stateRefresh}/>
+      </div>
     );
   }
 }
